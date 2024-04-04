7d1c01ae575a4bcc30643acc7e01f73d33487ca8
Naru-hub <1208ny0324@gmail.com>
2024-04-04 19:48:12 +0900

    タスクの詳細ページへ遷移するリンクを設定

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-# from . import views
 from .views import TaskList, TaskDetail
 urlpatterns = [
     path("", TaskList.as_view()),
-    path("task/<int:pk>/", TaskDetail.as_view())
+    path("task/<int:pk>/", TaskDetail.as_view(), name="task")
 ]

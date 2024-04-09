f1792fd56faf3eb32aee603a813588583c8a2261
Naru-hub <1208ny0324@gmail.com>
2024-04-09 09:36:58 +0900

    ログイン機能を実装

--- todoapp/views.py
+++ todoapp/views.py
@@ -4,6 +4,8 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
+from django.contrib.auth.views import LoginView
+
 from todoapp.models import Task
 
 # Create your views here.
@@ -30,5 +32,12 @@ class TaskDelete(DeleteView):
     fields = "__all__"
     success_url = reverse_lazy("tasks")
     context_object_name = "task"
+
+class TaskListLoginView(LoginView):
+    fields = "__all__"
+    template_name = "todoapp/login.html"
+
+    def get_success_url(self):
+        return reverse_lazy("tasks")
 
 

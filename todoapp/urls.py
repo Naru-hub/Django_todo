2e135df41c89348d2e292eba71c45b041cac13b1
Naru-hub <1208ny0324@gmail.com>
2024-04-10 09:39:47 +0900

    ログアウト機能を実装

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, TaskListLoginView
+from django.contrib.auth.views import LogoutView
+
 urlpatterns = [
     path("", TaskList.as_view(), name="tasks"),
     path("task/<int:pk>/", TaskDetail.as_view(), name="task"),
@@ -7,4 +9,5 @@ urlpatterns = [
     path("edit-task/<int:pk>/", TaskUpdate.as_view(), name="edit-task"),
     path("delete-task/<int:pk>/", TaskDelete.as_view(), name="delete-task"),
     path("login/", TaskListLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
 ]

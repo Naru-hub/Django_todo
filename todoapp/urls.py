3852ef95cc689c4b53fd107380d5e95ee31bd61a
Naru-hub <1208ny0324@gmail.com>
2024-04-14 10:11:53 +0900

    ユーザーの新規登録機能を実装

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, TaskListLoginView
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, TaskListLoginView, RegisterTodoApp
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
@@ -10,4 +10,5 @@ urlpatterns = [
     path("delete-task/<int:pk>/", TaskDelete.as_view(), name="delete-task"),
     path("login/", TaskListLoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
+    path("register/", RegisterTodoApp.as_view(), name="register"),
 ]
